80364c5e48ff720b5c7be2a0f2891adee8e18285
René Kulik <info@renekulik.de>
2017-06-28 22:46:42 +0200

    Extend frontend

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Testing in Laravel - Getting started!</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+<div class="col-md-6 col-md-offset-3">
+    @yield('content')
+</div>
+</body>
+</html>
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,3 +1,24 @@
-@foreach($tasks as $task)
-    <li>{{ $task->title }}</li>
-@endforeach
+@extends('layouts.master')
+
+@section('content')
+    <div class="col-md-12">
+        <h1>All Tasks</h1>
+        <ul class="list-group">
+            @foreach($tasks as $task)
+                <li class="list-group-item">{{ $task->title }}</li>
+            @endforeach
+        </ul>
+    </div>
+    <div class="col-md-12">
+        <h2>Create New Task</h2>
+        <form method="POST" action="/tasks">
+            {{ csrf_field() }}
+            <div class="form-group">
+                <input type="text" class="form-control" id="title" name="title" placeholder="Title">
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary">Create</button>
+            </div>
+        </form>
+    </div>
+@stop
