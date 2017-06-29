b8a888e1f43a3bb87be39a4678c82934ff10e5f2
René Kulik <info@renekulik.de>
2017-06-29 22:27:32 +0200

    Add resource key to task model

--- app/Task.php
+++ app/Task.php
@@ -8,11 +8,15 @@ use Illuminate\Database\Eloquent\Model;
  * Class Task
  * @package App
  *
+ * @property int $id
+ * @property string $title
+ *
  * @author René Kulik <info@renekulik.de>
  */
 class Task extends Model
 {
-    const TABLE_NAME = 'tasks';
+    public const TABLE_NAME = 'tasks';
+    public const RESOURCE_KEY = 'tasks';
 
     /**
      * @var string
