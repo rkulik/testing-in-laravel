e9b0fb190b75ec0d4b98759edf9aa1f6de713997
René Kulik <info@renekulik.de>
2017-06-30 00:02:28 +0200

    Throw exceptions in test environment

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -38,12 +38,17 @@ class Handler extends ExceptionHandler
     /**
      * Render an exception into an HTTP response.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
-     * @return \Illuminate\Http\Response
+     * @param \Illuminate\Http\Request $request
+     * @param Exception $exception
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws Exception
      */
     public function render($request, Exception $exception)
     {
+        if (app()->environment() === 'testing') {
+            throw $exception;
+        }
+
         return parent::render($request, $exception);
     }
 
