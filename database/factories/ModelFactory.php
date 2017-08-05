0eb239b383239773e4422beb128650a936258222
René Kulik <info@renekulik.de>
2017-08-05 16:41:41 +0200

    Move task factory to model factory file

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -12,13 +12,25 @@
 */
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-$factory->define(App\User::class, function (Faker\Generator $faker) {
-    static $password;
+$factory->define(
+    App\User::class,
+    function (Faker\Generator $faker) {
+        static $password;
 
-    return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
-        'remember_token' => str_random(10),
-    ];
-});
+        return [
+            'name' => $faker->name,
+            'email' => $faker->unique()->safeEmail,
+            'password' => $password ?: $password = bcrypt('secret'),
+            'remember_token' => str_random(10),
+        ];
+    }
+);
+
+$factory->define(
+    App\Task::class,
+    function (Faker\Generator $faker) {
+        return [
+            'title' => $faker->sentence(5),
+        ];
+    }
+);
